f5162bc64c3959d33504702e4c9baf495ed7f5d8
Ricardo M. Oliveira <ricardo.martinelli.oliveira@gmail.com>
2016-07-03 12:13:01 -0300

    Alterando a URI do servico REST de /rest/ para /api/

--- academix-model/src/main/java/br/nom/martinelli/ricardo/rest/RestApplication.java
+++ academix-model/src/main/java/br/nom/martinelli/ricardo/rest/RestApplication.java
@@ -3,6 +3,6 @@ package br.nom.martinelli.ricardo.rest;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.ApplicationPath;
 
-@ApplicationPath("/rest")
+@ApplicationPath("/api")
 public class RestApplication extends Application {
 }
--- academix-model/src/main/java/br/nom/martinelli/ricardo/service/EnderecoListResource.java
+++ academix-model/src/main/java/br/nom/martinelli/ricardo/service/EnderecoListResource.java
@@ -6,35 +6,61 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import br.nom.martinelli.ricardo.model.Endereco;
-import br.nom.martinelli.ricardo.types.UF;
 
-@Path("/")
+@Path("enderecos")
 @Stateless
 public class EnderecoListResource {
-	
+
 	@PersistenceContext
 	private EntityManager em;
-	
+
 	@GET
-	@Path("/alunos")
+	@Path("")
 	@Produces("application/json")
 	public List<Endereco> getEnderecos() {
 		Query q = em.createQuery("SELECT e FROM Endereco e");
 		List<Endereco> enderecos = q.getResultList();
-		Endereco end = new Endereco();
-		end.setId(1L);
-		end.setLogradouro("Avenida Doutor Arnaldo");
-		end.setCep("01235090");
-		end.setBairro("Lapa");
-		end.setCidade("Sao Paulo");
-		end.setUf(UF.SP);
-		enderecos.add(end);
 		return enderecos;
+	}
+	
+	@POST
+	@Path("")
+	@Consumes("application/json")
+	public void adicionaEndereco(Endereco endereco) {
+		em.persist(endereco);
+	}
+	
+	@PUT
+	@Path("")
+	@Consumes("application/json")
+	public void alteraEndereco(Endereco endereco) {
+		em.merge(endereco);
+	}
+	
+
+	@GET
+	@Path("{id}")
+	@Produces("application/json")
+	public Endereco getEndereco(@PathParam("id") Long id) {
+		Endereco endereco = em.find(Endereco.class, id);
+		return endereco;
+	}
+
+	@DELETE
+	@Path("{id}")
+	public void removeEndereco(@PathParam("id") Long id) {
+		Endereco endereco = em.find(Endereco.class, id);
+		em.remove(endereco);
 	}
 
 }
