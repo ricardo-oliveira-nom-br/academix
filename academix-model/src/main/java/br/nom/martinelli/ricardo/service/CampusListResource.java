616769283fae651d6da97b8edce0c1f961d80b24
Ricardo M. Oliveira <ricardo.martinelli.oliveira@gmail.com>
2016-07-17 21:12:58 -0300

    REST de Campus e Curso

---
+++ academix-model/src/main/java/br/nom/martinelli/ricardo/service/CampusListResource.java
@@ -0,0 +1,66 @@
+package br.nom.martinelli.ricardo.service;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import br.nom.martinelli.ricardo.model.Campus;
+
+@Path("campus")
+@Stateless
+public class CampusListResource {
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	@GET
+	@Path("")
+	@Produces("application/json")
+	public List<Campus> getCampus() {
+		Query q = em.createQuery("SELECT e FROM Campus e");
+		List<Campus> campus = q.getResultList();
+		return campus;
+	}
+	
+	@POST
+	@Path("")
+	@Consumes("application/json")
+	public void adicionaCampus(Campus campus) {
+		em.persist(campus);
+	}
+	
+	@PUT
+	@Path("")
+	@Consumes("application/json")
+	public void alteraCampus(Campus campus) {
+		em.merge(campus);
+	}
+	
+
+	@GET
+	@Path("{id}")
+	@Produces("application/json")
+	public Campus getCampus(@PathParam("id") Long id) {
+		Campus campus = em.find(Campus.class, id);
+		return campus;
+	}
+
+	@DELETE
+	@Path("{id}")
+	public void removeCampus(@PathParam("id") Long id) {
+		Campus campus = em.find(Campus.class, id);
+		em.remove(campus);
+	}
+
+}
---
+++ academix-model/src/main/java/br/nom/martinelli/ricardo/service/CursoListResource.java
@@ -0,0 +1,66 @@
+package br.nom.martinelli.ricardo.service;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import br.nom.martinelli.ricardo.model.Curso;
+
+@Path("curso")
+@Stateless
+public class CursoListResource {
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	@GET
+	@Path("")
+	@Produces("application/json")
+	public List<Curso> getCursos() {
+		Query q = em.createQuery("SELECT e FROM Curso e");
+		List<Curso> curso = q.getResultList();
+		return curso;
+	}
+	
+	@POST
+	@Path("")
+	@Consumes("application/json")
+	public void adicionaCurso(Curso curso) {
+		em.persist(curso);
+	}
+	
+	@PUT
+	@Path("")
+	@Consumes("application/json")
+	public void alteraCurso(Curso curso) {
+		em.merge(curso);
+	}
+	
+
+	@GET
+	@Path("{id}")
+	@Produces("application/json")
+	public Curso getCurso(@PathParam("id") Long id) {
+		Curso curso = em.find(Curso.class, id);
+		return curso;
+	}
+
+	@DELETE
+	@Path("{id}")
+	public void removeCurso(@PathParam("id") Long id) {
+		Curso curso = em.find(Curso.class, id);
+		em.remove(curso);
+	}
+
+}
